c8760871c24567a3ad57683cb3ad0cc665f8255a
Eliz <elizso200@gmail.com>
2020-04-06 15:53:06 +0800

    Update bt5.c

--- basicTask/bt5.c
+++ basicTask/bt5.c
@@ -4,14 +4,35 @@
 
 int main()
 {
-    float a,b;
+    float a, b;
     char op;
 
     printf("Enter a simple math expression with two operands (Example: 3 + 2)\nSeparate them with a space\n");
     printf("(Operator supported: +  -  *  / ^)\n\n");
 
     //you code here...
+    scanf("%f %c %f",&a, &op, &b);
+    switch(op){
+        case '+':
+            printf("Result: %0.2f + %0.2f = %0.2f \n", a, b, a+b);
+            break;
+        case '-':
+            printf("Result: %0.2f - %0.2f = %0.2f \n", a, b, a-b);
+            break;
+        case '*':
+            printf("Result: %0.2f * %0.2f = %0.2f \n", a, b, a*b);
+            break;
+        case '/':
+            printf("Result: %0.2f / %0.2f = %0.2f \n", a, b, a/b);
+            break;
+        case '^':
+            printf("Result: %0.2f ^ %0.2f = %0.2f \n", a, b, pow(a,b));
+            break;
+        default:
+            printf("Result: %0.2f %c %0.2f = Error! operator is not correct. \n", a, op, b);
 
-    system("pause");
+    }
+
+    system("wait");
     return 0;
 }
